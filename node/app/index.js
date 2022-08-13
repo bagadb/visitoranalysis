054acf06c1585ac10c363b6027c16248c85315c2
bagadb <shreyaslolage@gmail.com>
2022-08-13 07:18:55 +0000

    moving workflow to desktop

--- node/app/index.js
+++ node/app/index.js
@@ -5,13 +5,11 @@ const pool = new Pool()
 
 var server = express()
 
-var myvar = 'nothing yet';
-
 server.get('/', function (req, res) {
-	pool.query('INSERT INTO visitors_list values ( ' + req.ip + ' ) ON CONFLICT ( visitor_ip ) DO UPDATE SET count = visitors_list.count + 1; SELECT * FROM visitors_list;', (err, res) => {
-		myvar = res;
-	})
-	res.send('psql says ' + JSON.stringify(myvar) )
+	const text = 'INSERT INTO visitors_list(visitor_ip) VALUES ( $1 ) ON CONFLICT ( visitor_ip ) DO UPDATE SET count = visitors_list.count + 1'
+	const values = [ req.ip ];
+	answer = 'nothing yet'
+	res.send( answer );
 })
 
 server.listen(8080)
