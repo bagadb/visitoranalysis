a40033f511162bb2fe1060f0daa58aa591b150b9
Shreyas Lolage <shreyaslolage@gmail.com>
2022-12-22 20:21:51 +0530

    UI, basic functionality finished

--- postgres/init-table.sh
+++ postgres/init-table.sh
@@ -3,8 +3,9 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE TABLE visitors_list(
-	visitor_ip varchar(20) NOT NULL,
-	count integer DEFAULT 1,
+	visitor_ip VARCHAR(50) NOT NULL,
+	count INTEGER DEFAULT 1,
+	whois_data JSON,
 	PRIMARY KEY(visitor_ip)
 	);
 EOSQL
